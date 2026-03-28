f70454d5c7452511bd50cb7f88302927ea9ea32a
Ali Almuntheri <60100978@udst.edu.qa>
2026-03-28 20:28:56 +0300

    Add DBUtils.java

--- MovieTheatre/src/DBUtils.java
+++ MovieTheatre/src/DBUtils.java
@@ -1,2 +1,28 @@
+import java.sql.*;
+
 public class DBUtils {
+    private static String url = "jdbc:mysql://localhost:3306/dacs3203";
+    private static String appUsername = "root";
+    private static String appPassword = "";
+
+    public static Connection establishConnection() {
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(url, appUsername, appPassword);
+            System.out.println("Connection Successful");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return con;
+    }
+
+    public static void closeConnection(Connection con, Statement stmt) {
+        try {
+            if (stmt != null) stmt.close();
+            if (con != null) con.close();
+            System.out.println("Connection is closed");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
