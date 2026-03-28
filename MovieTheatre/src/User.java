9db0074ce9dadad24d29c791d456095762206273
Ali Almuntheri <60100978@udst.edu.qa>
2026-03-28 20:22:46 +0300

    Add User.java

--- MovieTheatre/src/User.java
+++ MovieTheatre/src/User.java
@@ -1,4 +1,42 @@
-package PACKAGE_NAME;
-
 public class User {
+
+    private int id;
+    private String username;
+    private String passwordHash;
+    private String role;
+    private String firstName;
+    private String lastName;
+
+    public User(int id, String username, String passwordHash, String role, String firstName, String lastName) {
+        this.id = id;
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.role = role;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
 }
