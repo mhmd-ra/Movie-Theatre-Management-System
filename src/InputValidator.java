c8b52a2692a883be39c11123125e9e7acf17faf2
Mohammad Rabiei <60301704@udst.edu.qa>
2026-03-29 19:39:55 +0300

    added input validator for username and name

---
+++ src/InputValidator.java
@@ -0,0 +1,23 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputValidator {
+
+    // acceptable Username is made of: letters, numbers, underscore, 2-32 characters
+
+    public static boolean validateUsername(String username) {
+        String regex = "^[a-zA-Z0-9_]{2,32}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(username);
+        return matcher.matches();
+    }//end of vvalidateUsername
+
+
+    // Name: starts with uppercase, followed by lowercase letters, hyphens
+    public static boolean validateName(String name) {
+        String regex = "^[A-Z][a-z'-]+$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(name);
+        return matcher.matches();
+    }
+}
