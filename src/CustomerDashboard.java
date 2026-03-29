13c74e67973a58627100c0f0b59a3c917537908c
Mohammad Rabiei <60301704@udst.edu.qa>
2026-03-29 20:35:33 +0300

    added import to prepare customer dashboard

--- src/CustomerDashboard.java
+++ src/CustomerDashboard.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class CustomerDashboard {
+
+    private Stage primaryStage;
+    private User currentUser;
+
+    public CustomerDashboard(Stage primaryStage, User currentUser) {
+        this.primaryStage = primaryStage;
+        this.currentUser = currentUser;
+    }
+    
 }
